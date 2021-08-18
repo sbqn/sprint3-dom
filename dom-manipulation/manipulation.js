e79ebd90212ff539f6f02b4065426367bc10cc90
sbqn <88091465+sbqn@users.noreply.github.com>
2021-08-18 21:31:13 +0700

    update rabu

--- dom-manipulation/manipulation.js
+++ dom-manipulation/manipulation.js
@@ -1,11 +1,23 @@
 // Element Manipulation
 //element.innerHTML --> mengubah isi dari tag yang sudah dipilih.
+const judul = document.getElementById('judul');
+judul.innerHTML = "Dojoboxxx.id";
 
 //element.style.<property> --> mengubah syle dari elemen yang dipilih.
+const par1 = document.getElementById('p1');
+par1.style.color = "red";
+judul.style.color = "magenta";
+
 
 //element.setAttribute() --> menambahkan attribute pada HTML.
+const jojo = document.querySelector ('#b ul li:nth-child(1)');
+jojo.setAttribute('id','mentoor');
+
+const mentor = document.querySelector('#b p')
+mentor.setAttribute('id', 'mentorx')
 
 //element.classList --> memanipulasi kelas
+
 
 //classList.add() --> menambah kelas baru
 //classList.remove() --> menghilangkan kelas yang ada
--- dom-selection/selection.js
+++ dom-selection/selection.js
@@ -1,11 +1,18 @@
 //DOM Selection
 
 //getElementById() --> memilih elemen yang ada di dalam DOM berdasarkan id.
+const sectionA = document.getElementById('a');
 
 //getElementsByTagName --> memilih elemen yang ada di dalam DOM berdasarkan tag tertentu.
+const paragraph = document.getElementsByTagName('p');
+const paragraphIndex = document.getElementsByTagName('p')[1];
 
 //getElementsByClassName --> memilih elemen yang ada di dalam DOM berdasarkan class tertentu.
+const paragraph1 = document.getElementsByClassName('p1');
 
 //querySelector() --> mencari elemen berdasarkan selektor
+const jojo1 = document.querySelector('section#b ul li:nth-child(1)');
+const jojo = document.querySelector('#b ul li:nth-child(1)');
 
 //querySelectorAll() --> mencari elemen berdasarkan selector
+const mentor = document.querySelectorAll('#b ul li');
